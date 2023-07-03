f2ba589b744df5021d37bfd9ae4fba851a43c213
TolikCrypto <138507542+TolikCrypto@users.noreply.github.com>
2023-07-03 17:17:05 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import pyautogui as pag
+
+pag.size()
+sizes = pag.size()
+print(sizes)
+
+while True:
+    pag.click()
+    pag.moveTo(1200, 930, 2)
+    pag.click()
+    pag.moveTo(1150, 1070, 2)
+    pag.click()
+    pag.moveTo(1200, 930, 2)
+    pag.click()
+    pag.moveTo(1180, 1070, 2)
+    pag.click()
+    pag.moveTo(1200, 930, 2)
+    pag.click()
+    pag.moveTo(1130, 1070, 2)
+
